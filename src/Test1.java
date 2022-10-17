f6a52ca780ddb7f867f15b664579abd8c973906d
yongg <yonggangdong@qq.com>
2022-10-17 16:47:52 +0800

    test1

--- src/Test1.java
+++ src/Test1.java
@@ -2,5 +2,9 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("hello a git");
         System.out.println(123);
+        String a = "aaa";
+        String b = "ccc";
+        String c = a+b;
+        System.out.println(c);
     }
 }
